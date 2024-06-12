98cd8f580c63047c68c10d50db1366269a120398
Eizaku Asai <vmr4ku@virginia.edu>
2024-06-12 15:12:26 -0400

    Created urls.py for project

---
+++ mysite/project/urls.py
@@ -0,0 +1,10 @@
+"""
+URLs for project app
+"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+]
