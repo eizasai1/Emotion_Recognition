4fba19fc876307598cfc630c5a6bd9fa9aabb875
Eizaku Asai <vmr4ku@virginia.edu>
2024-06-12 14:57:03 -0400

    Added dotenv usage for environment variables

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =
+IS_DEPLOYED = "PLACEHOLDER" in os.environ
+if IS_DEPLOYED:
+    SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+else:
+    from dotenv import load_dotenv
+    load_dotenv()
+    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if IS_DEPLOYED:
+    DEBUG = False
+else:
+    if os.getenv("DEBUG") == "True":
+        DEBUG = True
+    else:
+        DEBUG = False
 
 ALLOWED_HOSTS = []
 
